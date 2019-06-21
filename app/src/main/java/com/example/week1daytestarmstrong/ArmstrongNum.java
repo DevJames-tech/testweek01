299b4949a37709146bb5842904a107e1230d3398
consultant <james.calvin@mobileappscompany.com>
2019-06-21 18:04:44 -0400

    Test

---
+++ app/src/main/java/com/example/week1daytestarmstrong/ArmstrongNum.java
@@ -0,0 +1,23 @@
+package com.example.week1daytestarmstrong;
+
+public class ArmstrongNum {
+
+    public void strongBox(String number) {
+        int numb1;
+        int numb2;
+        int numb3;
+
+        String num = number.toString();
+
+        for (int i = 0; i < number.length(); i++) {
+
+            char mynum = num.split("|");
+            //split by number
+            //takes each number and it will be read int nubers
+            //Will be taken and taken to the 3rd cube
+            //then added it
+
+        }
+
+    }
+}
